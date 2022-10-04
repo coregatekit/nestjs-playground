da82531ed2235c6379ec86acbe92e004406d7cc6
Joe Kim <joesm.genie@gmail.com>
2022-10-04 16:01:24 +0700

    Implement caching with client ip

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Ip } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello(@Ip() ip): Promise<string> {
+    return await this.appService.getHello(ip);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  constructor(@Inject(CACHE_MANAGER) private readonly cacaheManager: Cache) {}
+
+  async getHello(ip: any): Promise<string> {
+    await this.cacaheManager.set('test', { ip: '192.168.1.1 ' });
+    await this.cacaheManager.set('client-ip', ip);
+    const cachedItem = await this.cacaheManager.get('test');
+    const cachedIp = await this.cacaheManager.get('client-ip');
+    console.log(cachedIp);
+    console.log(cachedItem);
     return 'Hello World!';
   }
 }
